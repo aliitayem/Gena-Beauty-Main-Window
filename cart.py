d4ed87b0daaeeb35bad51152969be49e84899807
aliitayem <111657014+aliitayem@users.noreply.github.com>
2023-09-29 21:20:23 -0500

    Update cart.py
    requires new "inventory.report" table. *Adding quantities to keep track of inventory*

--- cart.py
+++ cart.py
@@ -1,84 +1,23 @@
-from datetime import date, time, datetime
-import tkinter
-import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+from tkinter import colorchooser
+import tkinter.font as tkFont
+import tkinter as tk
 import mysql.connector
 import os
-import tk as tk
-
-cart = Tk()
-cart.title("Gena Beauty Supply | Cart Mode")
-cart.geometry("1500x1000")
-
-# Add Some Style
-style = ttk.Style()
-
-# Pick A Theme
-style.theme_use('default')
-
-# Configure the Treeview Colors
-style.configure("Treeview",
-                background="#D3D3D3",
-                foreground="black",
-                rowheight=25,
-                fieldbackground="#D3D3D3")
-
-# Change Selected Color
-style.map('Treeview',
-          background=[('selected', "#347083")])
-
-# Create a Treeview Frame
-tree_frame = Frame(cart)
-tree_frame.pack(pady=10)
-
-# Create a Treeview Scrollbar
-tree_scroll = Scrollbar(tree_frame)
-tree_scroll.pack(side=RIGHT, fill=Y)
-
-# Create The Treeview
-my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
-my_tree.pack()
-
-# Configure the Scrollbar
-tree_scroll.config(command=my_tree.yview())
-
-# Define Our Treeview Columns
-my_tree['columns'] = ('Name', 'Price')
-
-# Format Treeview Columns
-my_tree.column("#0", width=0, stretch=NO)
-my_tree.column("Name", anchor=W, width=800)
-my_tree.column("Price", anchor=W, width=50)
-
-# Create Treeview Headings
-my_tree.heading("#0", text="")
-my_tree.heading("Name", text="")
-my_tree.heading("Price", text="")
 
 
-# Display Treeview
-my_tree.pack()
-
-my_tree.configure()
-
-mydb = mysql.connector.connect(
+db = mysql.connector.connect(
     host="localhost",
     user="root",
-    database="inventory",
-    passwd="root"
+    passwd="root",
+    database="inventory"
 )
 
-mycursor = mydb.cursor()
-
-
-# Create the widgets
-enter_barcode_label = Label(cart, text="Enter Barcode:")
-enter_barcode_entry = Entry(cart)
-add_to_cart_btn = Button(cart, text="Add to Cart")
+mycursor = db.cursor()
 
 def query_database():
-
     for record in my_tree.get_children():
         my_tree.delete(record)
 
@@ -102,58 +41,123 @@ def query_database():
         if count % 2 == 0:
             my_tree.insert(parent='', index='end', iid=count, text='',
                            values=(
-                               record[4], record[1]),
-                           tags=('evenrow',))
+                               record[0], record[1], record[2], record[3], record[4]))
         else:
             my_tree.insert(parent='', index='end', iid=count, text='',
                            values=(
-                               record[4], record[1]),
-                           tags=('oddrow',))
-        # increment counter
+                               record[0], record[1], record[2], record[3], record[4]))
         count += 1
-
     db.commit()
     db.close()
 
 
-# Define the action when the button is clicked
-def add_to_cart():
-    # Get the barcode
-    barcode = enter_barcode_entry.get()
+def add_To_Cart(self):
 
-    # Create a cursor to interact with the database
-    mycursor = mydb.cursor()
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="inventory"
+    )
 
-    # Select the data from the products table
-    sql = "SELECT * FROM products WHERE barcode = %s"
-    val = (barcode,)
-    mycursor.execute(sql, val)
+    mycursor = db.cursor()
 
-    # Get the data
-    product = mycursor.fetchone()
+    barcode = barcode_entry.get()
 
-    # Insert the product into the carts table
-    sql = "INSERT INTO carts (name, price, quantity, barcode) VALUES (%s, %s, %s, %s)"
-    val = (product[2], product[7], product[9], product[1])
-    mycursor.execute(sql, val)
-    mydb.commit()
+    mycursor.execute("SELECT price, barcode, name FROM inventory.products WHERE barcode = %s", (barcode,))
+    result = mycursor.fetchone()
 
-    # Update Tree View
-    my_tree.delete(*my_tree.get_children())
-    # Query Database And Refresh Treeview
+    if result:
+        price, barcode, name = result
+
+        mycursor.execute("INSERT INTO inventory.carts (price, quantity, barcode, name) VALUES (%s, 1, %s, %s)  ON DUPLICATE KEY UPDATE quantity = quantity + 1", (price, barcode, name))
+        print("Item Added To Cart Successfully")
+    else:
+        print("Item Not Found")
+
+    db.commit()
+    db.close()
+
+    barcode_entry.delete(0, END)
+
     query_database()
 
 
-# Configure the widgets
-enter_barcode_label.pack()
-enter_barcode_entry.pack()
-add_to_cart_btn.pack()
-reset_button = Button(cart, text="Reset", command=query_database)
-reset_button.pack()
+def complete_Order():
 
-# Call the function when the button is clicked
-add_to_cart_btn.configure(command=add_to_cart)
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        database="inventory"
+    )
+
+    mycursor = db.cursor()
+
+    mycursor.execute("SELECT barcode, quantity FROM inventory.carts")
+    records = mycursor.fetchall()
+
+
+    for barcode, quantity in records:
+        mycursor.execute("INSERT INTO inventory.report (barcode, quantity) VALUES (%s, %s) ON DUPLICATE KEY UPDATE quantity = report.quantity + %s", (barcode, quantity, quantity))
+
+    mycursor.execute("DELETE FROM inventory.carts")
+
+    db.commit()
+    db.close()
+
+    query_database()
+
+
+window = tk.Tk()
+window.title("Cart")
+
+# Create a Treeview Frame
+tree_frame = Frame(window)
+tree_frame.pack(pady=10)
+
+# Create a Treeview Scrollbar
+tree_scroll = Scrollbar(tree_frame)
+tree_scroll.pack(side=RIGHT, fill=Y)
+
+# Create The Treeview
+my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
+my_tree.pack()
+
+# Configure the Scrollbar
+tree_scroll.config(command=my_tree.yview)
+
+# Define Our Columns
+my_tree['columns'] = (
+    "ID", "Barcode", "Name", "Price", "Quantity")
+
+# Format Our Columns
+my_tree.column("#0", width=0, stretch=NO)
+my_tree.column("ID", anchor=CENTER, width=50)
+my_tree.column("Price", anchor=CENTER, width=140)
+my_tree.column("Quantity", anchor=CENTER, width=280)
+my_tree.column("Barcode", anchor=CENTER, width=80)
+my_tree.column("Name", anchor=CENTER, width=60)
+
+# Create Headings
+my_tree.heading("#0", text="", anchor=W)
+my_tree.heading("ID", text="ID", anchor=CENTER)
+my_tree.heading("Price", text="Barcode", anchor=CENTER)
+my_tree.heading("Quantity", text="Name", anchor=CENTER)
+my_tree.heading("Barcode", text="Price", anchor=CENTER)
+my_tree.heading("Name", text="Quantity", anchor=CENTER)
+
+barcode_entry = Entry(window)
+barcode_entry.pack()
+
+complete_order_button = tk.Button(window, text="Complete Order", command=complete_Order)
+complete_order_button.pack()
+
+add_to_cart_button = tk.Button(window, text="Add To Cart", command=add_To_Cart)
+add_to_cart_button.pack()
+
+window.bind("<Return>", add_To_Cart)
 
 query_database()
 
-cart.mainloop()
+window.mainloop()
